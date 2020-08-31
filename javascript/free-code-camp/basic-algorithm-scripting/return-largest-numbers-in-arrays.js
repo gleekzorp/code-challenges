b9f3f290c1432f8961c3bb3297254a2128fd0ddf
Daniel Floyd <gleek.dan@gmail.com>
2020-08-31 10:37:38 -0600

    uncommented original

--- javascript/free-code-camp/basic-algorithm-scripting/return-largest-numbers-in-arrays.js
+++ javascript/free-code-camp/basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -12,24 +12,24 @@
   // Continue this until all done
 // return result
 
-// function largestOfFour(arr) {
-//   let result = []
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-//     let currentLargestNumber = arr[i][0]
-//     for (let j = 0; j < arr[i].length; j++) {
-//       console.log(arr[i][j])
-//       if (arr[i][j] > currentLargestNumber) {
-//         currentLargestNumber = arr[i][j]
-//       }
-//     }
-//     result.push(currentLargestNumber)
-//   }
+function largestOfFour(arr) {
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    let currentLargestNumber = arr[i][0]
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j])
+      if (arr[i][j] > currentLargestNumber) {
+        currentLargestNumber = arr[i][j]
+      }
+    }
+    result.push(currentLargestNumber)
+  }
 
-//   return result;
-// }
+  return result;
+}
 
-// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
 
 
