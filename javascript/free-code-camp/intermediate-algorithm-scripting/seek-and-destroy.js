18d472682b5786d71b1ef254f960485b46793751
gleekzorp <gleek.dan@gmail.com>
2020-09-22 22:34:08 -0600

    added some info on my npm package and built out seek and destroy with filter

--- javascript/free-code-camp/intermediate-algorithm-scripting/seek-and-destroy.js
+++ javascript/free-code-camp/intermediate-algorithm-scripting/seek-and-destroy.js
@@ -1,18 +1,12 @@
-// loop through and pop off elements it found
-
 // function destroyer(arr) {
-//   console.log(Object.values(arguments))
-//   let arrayLength = arr.length
+//   let argumentsArray = Object.values(arguments).splice(1)
 //   let notFound = []
-//   let found = []
-//   for (let i = 0; i < arrayLength; i ++) {
-//     if (arr[i] === arguments[1] || arr[i] === arguments[2]) {
+//   for (let i = 0; i < arr.length; i ++) {
+//     if (!argumentsArray.includes(arr[i])) {
 //       notFound.push(arr[i])
-//     } else {
-//       found.push(arr[i])
 //     }
 //   }
-//   return found;
+//   return notFound;
 // }
 
 // console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
@@ -20,32 +14,11 @@
 
 function destroyer(arr) {
   let argumentsArray = Object.values(arguments).splice(1)
-  let found = []
-  for (let i = 0; i < arr.length; i ++) {
-    if (!argumentsArray.includes(arr[i])) {
-      found.push(arr[i])
+  return arr.filter((item) => {
+    if (!argumentsArray.includes(item)) {
+      return item
     }
-  }
-  return found;
+  })
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
-
-// function destroyer(arr) {
-//   console.log(Object.values(arguments))
-//   let arrayLength = arr.length
-//   let notFound = []
-//   let found = []
-//   for (let i = 0; i < arrayLength; i ++) {
-//     for (let j = 1; j < arguments.length; j++) {
-//       if (arr[i] === arguments[j]) {
-//         notFound.push(arr[i])
-//       } else {
-//         found.push(arr[i])
-//       }
-//     }
-//   }
-//   return found;
-// }
-
-// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
--- javascript/free-code-camp/intermediate-algorithm-scripting/sum-all-numbers-in-a-range.js
+++ javascript/free-code-camp/intermediate-algorithm-scripting/sum-all-numbers-in-a-range.js
@@ -2,7 +2,9 @@
 // Find out which is bigger
 // loop through and add each number to the current total
 
-
+/////////////
+// Attempt 1
+////////////
 // function sumAll(arr) {
 //   let result = 0
 //   if (arr[0] > arr[1]) {
@@ -21,42 +23,41 @@
 // console.log(sumAll([1, 4]));
 
 
+//////////////
+// Refactor 1
+/////////////
+// function sumAll(arr) {
+//   let max = Math.max(...arr)
+//   let min = Math.min(...arr)
+//   let result = 0
+//   for (let i = min; i <= max; i++){
+//     result += i
+//   }
 
-
-function sumAll(arr) {
-  let max = Math.max(...arr)
-  let min = Math.min(...arr)
-  let result = 0
-  for (let i = min; i <= max; i++){
-    result += i
-  }
-
-  return result
-}
-
-console.log(sumAll([4, 1]));
-console.log(sumAll([1, 4]));
-
-
-
-////////////
-// Go Crazy
-///////////
-// const sumAll = (arr) => [...Array(Math.max(...arr)).keys()].map(i => i + 1).reduce((total,item) => total + item)
+//   return result
+// }
 
 // console.log(sumAll([4, 1]));
 // console.log(sumAll([1, 4]));
-// console.log(sumAll([5, 10]));
 
-// newArray = new Array(Math.max([4,1])).fill(Math.max([4,1], Math.min([4,1])))
-// newArray = new Array(Math.max([4,1]))
-// currentArray = [4,1]
-// newArray = Math.max(...currentArray)
 
-// let max = Math.max(...currentArray)
-// let min = Math.min(...currentArray)
-// let newArray = [...Array(max).keys()].map(i => i + 1).reduce((total,item) => total + item)
-// let newArray = Array.from(Array(max).keys()).map(i => i + 1)
-// console.log(newArray)
 
-// console.log([4,1].sort().reduce((total,item) => total + item))
+////////////////////////
+// Using my npm package
+///////////////////////
+// https://www.npmjs.com/package/ranges-numbers-alphabets
+function sumAll(arr) {
+  let result = range(Math.min(...arr), Math.max(...arr))
+  return result.reduce((total, num) => total + num);
+}
+
+const range = (start, stop, step = 1) => {
+  let result = []
+  for (let i = start; i <= stop; i += step) {
+    result.push(i)
+  }
+  return result
+}
+
+
+console.log(sumAll([1, 4]));
