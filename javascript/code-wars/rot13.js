c989a3c578cadec6fd33421d18bd16049b2fd299
Daniel Floyd <gleek.dan@gmail.com>
2020-10-15 12:16:50 -0600

    added cory's solution

--- javascript/code-wars/rot13.js
+++ javascript/code-wars/rot13.js
@@ -76,5 +76,27 @@
 //   })
 // }
 
+
+
+
+////////////////
+// Cory Solution
+////////////////
+function rot13(message) {
+  function inRange(x, min, max) {
+    return (x - min) * (x - max) <= 0;
+  }
+  return message.split("").map((letter) => {
+    let decNum = letter.charCodeAt(0);
+    if (!inRange(decNum, 65, 90) && !inRange(decNum, 97, 122)) {
+      return String.fromCharCode(decNum);
+    } else {
+      return inRange(decNum, 65, 77) || inRange(decNum, 97, 109)
+        ? String.fromCharCode(decNum + 13)
+        : String.fromCharCode(decNum - 13);
+    }
+  }).join('');
+}
+
 console.log(rot13('test'))
 console.log(rot13('!test'))
