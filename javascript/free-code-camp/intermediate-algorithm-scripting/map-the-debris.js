51af0170c71342c42b3a65f49826ed430a666a49
Daniel Floyd <gleek.dan@gmail.com>
2020-11-17 20:32:21 -0700

    screenshots and map-the-debris added

---
+++ javascript/free-code-camp/intermediate-algorithm-scripting/map-the-debris.js
@@ -0,0 +1,10 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/map-the-debris
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) // should return [{name: "sputnik", orbitalPeriod: 86400}].
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
