e07fb625b93fdf69734813afca679fcba245b56a
Daniel Floyd <gleek.dan@gmail.com>
2020-12-06 17:29:38 -0700

    regex complete

--- javascript/free-code-camp/algorithms-and-data-structures-projects/telephone-number-validator.js
+++ javascript/free-code-camp/algorithms-and-data-structures-projects/telephone-number-validator.js
@@ -1,4 +1,13 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
+// https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-telephone-number-validator/16090
+
+/////////////////
+// Helpful Tools
+////////////////
+// https://regex101.com/
+// https://www.regexpal.com/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+// https://cheatography.com/davechild/cheat-sheets/regular-expressions/
 
 // 555-555-5555
 // (555)555-5555
@@ -19,18 +28,15 @@ function telephoneCheck(str) {
   // const regex = RegExp(/[0-9-\s\(\)]/g)
   // const regex = RegExp(/([0-9-\s\(\)])(?=\())/g)
   // const regex = RegExp(/\b\d{3,4}\b/g)
-  const regex = RegExp(/(\b\d{3,4}\b)/g)
+  // const regex = /(\d{3})-(\d{3})-(\d{4})/gm;
+  // const regex = /(\(?)(\d{3})(-|\))(\s?)(\d{3})-(\d{4})/gm;
+  // const regex = /(\(?)(\d{3})(-?|\)?|\s?)(\d{3})(\s?|-)(\d{4})/gm;
+  // const regex = /(1?)(\s?)(\(?)(\d{3})(-?|\)?|\s?)(\d{3})(\s?|-)(\d{4})/gm;
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
   let result = regex.test(str)
   return result;
 }
 
-console.log(telephoneCheck('33')) // should return a boolean.
-console.log(telephoneCheck('333')) // should return a boolean.
-console.log(telephoneCheck('3333')) // should return a boolean.
-// console.log(telephoneCheck("(")) // should return a boolean.
-// console.log(telephoneCheck(")")) // should return a boolean.
-// console.log(telephoneCheck("-")) // should return a boolean.
-// console.log(telephoneCheck("(555)")) // should return a boolean.
 // console.log(telephoneCheck("(555)555-5555")) // should return a false.
 // console.log(telephoneCheck("555-555-5555")) // should return a boolean.
 // console.log(telephoneCheck("1 555-555-5555")) //  should return true.
