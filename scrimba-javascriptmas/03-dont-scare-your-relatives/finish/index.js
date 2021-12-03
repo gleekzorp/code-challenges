2a468a12df8f1e7144ccabcbe78ebc32d0312be1
Daniel <gleek.dan@gmail.com>
2021-12-03 11:15:43 -0700

    Add 03 initial task.  No stretch goals

--- scrimba-javascriptmas/03-dont-scare-your-relatives/finish/index.js
+++ scrimba-javascriptmas/03-dont-scare-your-relatives/finish/index.js
@@ -5,7 +5,12 @@ function fix() {
   // Task:
   // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
   // - Run the function when the Fix button is clicked.
+  greeting.style.fontFamily = "Mountains of Christmas, cursive";
+  greeting.innerHTML = "🎅 Merry Christmas! 🎅";
+  btn.remove();
 }
+
+btn.addEventListener("click", fix);
 
 //Stretch goals:
 // - Add an extra theme, and the option to switch between them.
