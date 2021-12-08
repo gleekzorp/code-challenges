b58a641b08fdc127105e8921377fffa011babbf7
Daniel <gleek.dan@gmail.com>
2021-12-07 23:18:27 -0700

    Add 08 bell animator initial task and stretch goal finished

--- scrimba-javascriptmas/08-bell-animator/finish/index.js
+++ scrimba-javascriptmas/08-bell-animator/finish/index.js
@@ -1,3 +1,4 @@
+const bell = document.getElementById("bell");
 const playBtn = document.getElementById("play-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const stopBtn = document.getElementById("stop-btn");
@@ -7,18 +8,31 @@ const audio = new Audio(
 playBtn.addEventListener("click", play);
 pauseBtn.addEventListener("click", pause);
 stopBtn.addEventListener("click", stop);
+audio.addEventListener("ended", () => {
+  stop();
+});
 
 function play() {
   audio.play();
+  animateBell();
 }
 
 function pause() {
   audio.pause();
+  bell.classList.add("paused");
 }
 
 function stop() {
   audio.pause();
   audio.currentTime = 0;
+  bell.classList.remove("animate", "paused");
+}
+
+function animateBell() {
+  if (bell.classList.contains("paused")) {
+    bell.classList.remove("paused");
+  }
+  bell.classList.add("animate");
 }
 
 // Task:
