07a018bb8d7b3681853d29350a72582665f36608
Daniel <gleek.dan@gmail.com>
2021-12-07 22:42:00 -0700

    Add 07 initial task finished code

--- scrimba-javascriptmas/07-play-a-christmas-song/finish/index.js
+++ scrimba-javascriptmas/07-play-a-christmas-song/finish/index.js
@@ -1,9 +1,25 @@
 const playBtn = document.getElementById("play-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const stopBtn = document.getElementById("stop-btn");
+const audio = new Audio(
+  "zapsplat_musical_large_sleigh_bells_med_shake_001_15366.mp3"
+);
 
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+
+playBtn.addEventListener("click", () => {
+  audio.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+  audio.pause();
+});
+
+stopBtn.addEventListener("click", () => {
+  audio.pause();
+  audio.currentTime = 0;
+});
 
 // Stretch goals:
 // - Add volume controls.
