503483bee0b1b103f3dea53fcb2fb988315865ea
Daniel <gleek.dan@gmail.com>
2021-12-09 22:43:09 -0700

    Add 10 initial task finished

--- scrimba-javascriptmas/10-festivity-loader/finish/index.js
+++ scrimba-javascriptmas/10-festivity-loader/finish/index.js
@@ -3,6 +3,20 @@ const meter = document.getElementById("meter");
 // Task:
 // Write a function to wire up the festivity loader to reflect how many days are remaining until Christmas!
 
+// TODO: This won't work unless you are in the month of December.  I will need to look into how to make this work for any month.
+function festivityLoader() {
+  const today = new Date();
+
+  const interval = setInterval(() => {
+    meter.value = meter.value + 0.25;
+    if (meter.value == today.getDate()) {
+      clearInterval(interval);
+    }
+  }, 10);
+}
+
+festivityLoader();
+
 // Stretch goals:
 // - Animate the loader.
 // - Change the colors depending on the meter's value.
