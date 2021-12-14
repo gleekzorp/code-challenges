42b3d25b5db14c5c5c8a0eb47aa11bf70d836053
Daniel <gleek.dan@gmail.com>
2021-12-13 22:52:43 -0700

    Add 12 initial task finished

--- scrimba-javascriptmas/12-christmas-guest-list/finish/index.js
+++ scrimba-javascriptmas/12-christmas-guest-list/finish/index.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+const form = document.querySelector("form");
 let guestList = document.getElementById("guest-list");
 let guests = [
   "Partner",
@@ -12,6 +13,29 @@ let guests = [
 // Tasks:
 // - Write the JS to render the Christmas day guest list in the guest-list element.
 // - Add the functionality to add new guests.
+
+function createGuestItem(name) {
+  const li = document.createElement("li");
+  li.textContent = name;
+  guestList.appendChild(li);
+}
+
+function renderGuests() {
+  guests.forEach((guest) => {
+    createGuestItem(guest);
+  });
+}
+renderGuests();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value === "") {
+    alert("Please enter a name");
+  } else {
+    createGuestItem(input.value);
+    input.value = "";
+  }
+});
 
 // Stretch goals:
 // - Add a button to remove the most recently added guests.
