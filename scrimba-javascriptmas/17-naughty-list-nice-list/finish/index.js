2d88cd10dbcad8e1372b6fecc674df09b3995db5
Daniel <gleek.dan@gmail.com>
2021-12-18 10:11:53 -0700

    Add 17 finished initial task

--- scrimba-javascriptmas/17-naughty-list-nice-list/finish/index.js
+++ scrimba-javascriptmas/17-naughty-list-nice-list/finish/index.js
@@ -1,7 +1,7 @@
 const niceList = document.getElementById("nice-list");
 const naughtyList = document.getElementById("naughty-list");
 const btn = document.getElementById("btn");
-// btn.addEventListener("click", sort)
+btn.addEventListener("click", sortAndDisplay);
 
 const sorteesArr = [
   {
@@ -24,6 +24,15 @@ const sorteesArr = [
 
 // Task:
 // - Write the JavaScript to sort the people in sorteesArr into the naughty and nice lists, according to whether they have been good or not. Then display the names in the relevant place in the DOM.
+function sortAndDisplay() {
+  sorteesArr.forEach((person) => {
+    if (person.hasBeenGood) {
+      niceList.innerHTML += `<li>${person.name}</li>`;
+    } else {
+      naughtyList.innerHTML += `<li>${person.name}</li>`;
+    }
+  });
+}
 
 // Stretch goals:
 // - Add the option to add new names to the sorteesArr.
