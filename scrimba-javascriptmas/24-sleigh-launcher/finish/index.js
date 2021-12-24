85a1428a39498836e92067ab39908710b452fe03
Daniel <gleek.dan@gmail.com>
2021-12-24 11:04:32 -0700

    Add 24 initial task finished

--- scrimba-javascriptmas/24-sleigh-launcher/finish/index.js
+++ scrimba-javascriptmas/24-sleigh-launcher/finish/index.js
@@ -1,9 +1,15 @@
 const crew = document.getElementById("crew");
 const btn = document.getElementById("btn");
 
+// slide-out-elliptic-top-bck
+
 // Task:
 // - Write a function to launch the sleigh!
 // - See CSS for more tasks.
+
+btn.addEventListener("click", () => {
+  crew.classList.add("slide-out-elliptic-top-bck");
+});
 
 // Stretch goals:
 // - Add a cheering sound when the sleigh takes off.
