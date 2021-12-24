fcd8b57f3289d2c71e2e175c9b59ee00d206b5c9
Daniel <gleek.dan@gmail.com>
2021-12-24 10:18:18 -0700

    Add 20 initial task finished

--- scrimba-javascriptmas/20-snowman-customiser/finish/index.js
+++ scrimba-javascriptmas/20-snowman-customiser/finish/index.js
@@ -1,7 +1,44 @@
 const inputs = document.querySelectorAll(".controls input");
+const eyeColorInput = document.querySelector("#eyeColorInput");
+const buttonColorInput = document.querySelector("#buttonColorInput");
+const noseColorInput = document.querySelector("#noseColorInput");
+
+const eyes = document.querySelectorAll(".eye");
+const buttons = document.querySelectorAll(".button");
+const nose = document.querySelector(".nose");
 
 // Task:
 // Write a function to update the snowman colors according to the colors selected from the pickers.
+
+eyeColorInput.addEventListener("input", updateColor);
+buttonColorInput.addEventListener("input", updateColor);
+noseColorInput.addEventListener("input", updateColor);
+
+function updateColor(e) {
+  const value = e.target.value;
+  // console.log(value);
+  // console.log(e.target.id);
+  // console.log("input id: ", e.target.id);
+  // console.log("input value: ", e.target.value);
+  // console.log("input type: ", e.target.type);
+  switch (e.target.id) {
+    case "eyeColorInput":
+      eyes.forEach((eye) => {
+        eye.style.backgroundColor = value;
+      });
+      break;
+    case "buttonColorInput":
+      buttons.forEach((button) => {
+        button.style.backgroundColor = value;
+      });
+      break;
+    case "noseColorInput":
+      nose.style.borderColor = `transparent transparent transparent ${value}`;
+      break;
+    default:
+      break;
+  }
+}
 
 // Stretch goals:
 // - Add other items eg scarf, arms, etc.
