c754aa905187b0c5c32a900a67c1af205e2ac2f5
Daniel <gleek.dan@gmail.com>
2021-12-24 10:53:06 -0700

    Add 23 initial task finished

--- scrimba-javascriptmas/23-customisable-santa-sign/finish/index.js
+++ scrimba-javascriptmas/23-customisable-santa-sign/finish/index.js
@@ -1,6 +1,14 @@
 // Task:
 // - Write a function to update the Santa Stop Here sign with the user's own text.
 
+const textInput = document.getElementById("text-input");
+
+textInput.addEventListener("keyup", () => {
+  const text = textInput.value;
+  const sign = document.getElementById("sign");
+  sign.innerHTML = `🎅 ${text} ☃️`;
+});
+
 // Stretch goals
 // - Add a min and max length to the message.
 // - Add interchangeable emojis.
