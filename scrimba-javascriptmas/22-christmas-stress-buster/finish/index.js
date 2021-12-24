3685444c1279682e9b6ca682f6c0d646072a61f9
Daniel <gleek.dan@gmail.com>
2021-12-24 10:45:15 -0700

    Add 22 initial task finished

--- scrimba-javascriptmas/22-christmas-stress-buster/finish/index.js
+++ scrimba-javascriptmas/22-christmas-stress-buster/finish/index.js
@@ -6,6 +6,31 @@ let timeout;
 // Task:
 // - Write a function to start the countdown timer on the button click and display it in the DOM. Stop the timer when it reaches 0.
 
+function startTimer() {
+  let time = 15 * 60;
+  let interval = setInterval(() => {
+    time--;
+    timer.innerHTML = showTime(time);
+    if (time === 0) {
+      clearInterval(interval);
+      timer.innerHTML = `<p>Time's up!</p>`;
+      affirmation.innerHTML = `<p>You are a good reindeer!</p>`;
+    }
+  }, 1000);
+}
+
+function showTime(allowedTime) {
+  let minutes = pad(Math.floor(allowedTime / 60));
+  let seconds = pad(allowedTime % 60);
+  return `${minutes}:${seconds}`;
+}
+
+function pad(number) {
+  return number < 10 ? `0${number.toString()}` : number;
+}
+
+btn.addEventListener("click", startTimer);
+
 // Stretch goals:
 // - Show a relaxing image in the background while the timer is running.
 // - Play relaxing music.
