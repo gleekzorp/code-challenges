f03d0428e2a892eedd05250016df41f490cbfa73
Daniel Floyd <gleek.dan@gmail.com>
2022-05-26 00:04:10 -0600

    Update: flip-card to have korean questions

--- scrimba-web-dev-weekly/flip-card/questions.js
+++ scrimba-web-dev-weekly/flip-card/questions.js
@@ -1,14 +1,162 @@
 questions = [
   {
-    question: "What does JSON stand for?",
-    answer: "JavaScipt Object Notation",
+    question: "개",
+    answer: "DOG",
   },
   {
-    question: "What does CRUD stand for?",
-    answer: "Create Read Update Delete",
+    question: "DOG",
+    answer: "개",
   },
   {
-    question: "What does OOP stand for?",
-    answer: "Object Oriented Programming",
+    question: "바지",
+    answer: "PANTS",
+  },
+  {
+    question: "PANTS",
+    answer: "바지",
+  },
+  {
+    question: "코",
+    answer: "NOSE",
+  },
+  {
+    question: "NOSE",
+    answer: "코",
+  },
+  {
+    question: "모자",
+    answer: "HAT",
+  },
+  {
+    question: "HAT",
+    answer: "모자",
+  },
+  {
+    question: "나무",
+    answer: "TREE",
+  },
+  {
+    question: "TREE",
+    answer: "나무",
+  },
+  {
+    question: "아기",
+    answer: "BABY",
+  },
+  {
+    question: "BABY",
+    answer: "아기",
+  },
+  {
+    question: "다리",
+    answer: "LEGS",
+  },
+  {
+    question: "LEGS",
+    answer: "다리",
+  },
+  {
+    question: "치마",
+    answer: "SKIRT",
+  },
+  {
+    question: "SKIRT",
+    answer: "치마",
+  },
+  {
+    question: "고래",
+    answer: "WHALE",
+  },
+  {
+    question: "WHALE",
+    answer: "고래",
+  },
+  {
+    question: "나비",
+    answer: "BUTTERFLY",
+  },
+  {
+    question: "BUTTERFLY",
+    answer: "나비",
+  },
+  {
+    question: "귀",
+    answer: "EAR",
+  },
+  {
+    question: "EAR",
+    answer: "귀",
+  },
+  {
+    question: "시계",
+    answer: "CLOCK",
+  },
+  {
+    question: "CLOCK",
+    answer: "시계",
+  },
+  {
+    question: "뇌",
+    answer: "BRAIN",
+  },
+  {
+    question: "BRAIN",
+    answer: "뇌",
+  },
+  {
+    question: "우유",
+    answer: "MILK",
+  },
+  {
+    question: "MILK",
+    answer: "우유",
+  },
+  {
+    question: "의사",
+    answer: "DOCTOR",
+  },
+  {
+    question: "DOCTOR",
+    answer: "의사",
+  },
+  {
+    question: "사과",
+    answer: "APPLE",
+  },
+  {
+    question: "APPLE",
+    answer: "사과",
+  },
+  {
+    question: "의자",
+    answer: "CHAIR",
+  },
+  {
+    question: "CHAIR",
+    answer: "의자",
+  },
+  {
+    question: "돼지",
+    answer: "PIG",
+  },
+  {
+    question: "PIG",
+    answer: "돼지",
+  },
+  {
+    question: "가위",
+    answer: "SCISSORS",
+  },
+  {
+    question: "SCISSORS",
+    answer: "가위",
+  },
+  {
+    question: "과자",
+    answer: "SNACK",
+  },
+  {
+    question: "SNACK",
+    answer: "과자",
   },
 ];
